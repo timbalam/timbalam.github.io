b9329a5d243ad11451581b04631aed4b93e5b29d
Tim Lamberton <tim.lamberton@gmail.com>
2018-09-17 20:11:47 +0200

    revised tutorial content and update interpreter engine

--- language/script/setup-goat-web.js
+++ language/script/setup-goat-web.js
@@ -1,18 +1,13 @@
-var codeboxes = document.querySelectorAll('textarea.codebox');
-function appendOutput(res) {
-  output.textContent += res + '\n';
-  scrollToBottom(output)
-};
 function setup(cb) {
   document.querySelectorAll('textarea.codebox').forEach(function (tb) {
     var o = tb.insertAdjacentElement('afterend',document.createElement('pre'));
     o.className = 'codebox-output';
     tb.onblur = function () {
-      cb(tb.value, o);
+      cb(o, tb.value);
     };
   });
 };
-/*setup(function(str1, str2) {
-  console.log(str1);
-  appendResult(str2);
+/*setup(function(o, str) {
+  console.log(str);
+  o.textContent = str;
 });*/
